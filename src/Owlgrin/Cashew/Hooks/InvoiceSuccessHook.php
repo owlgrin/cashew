6d454135cf15f11984f3621e56554551bc3ce95d
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-04-02 13:36:32 +0530

    fixed condition to activate the subscription

--- src/Owlgrin/Cashew/Hooks/InvoiceSuccessHook.php
+++ src/Owlgrin/Cashew/Hooks/InvoiceSuccessHook.php
@@ -5,14 +5,17 @@ use Owlgrin\Cashew\Invoice\StorableInvoice;
 use Owlgrin\Cashew\Storage\Storage;
 use Owlgrin\Cashew\Hooks\Hook;
 use Owlgrin\Cashew\Event\Event;
+use Owlgrin\Cashew\Cashew;
 
 class InvoiceSuccessHook implements Hook {
 
 	protected $storage;
+	protected $cashew;
 
-	public function __construct(Storage $storage)
+	public function __construct(Storage $storage, Cashew $cashew)
 	{
 		$this->storage = $storage;
+		$this->cashew = $cashew;
 	}
 
 	public function handle(Event $event)
@@ -20,13 +23,19 @@ class InvoiceSuccessHook implements Hook {
 		$invoice = $event->invoice();
 		$subscription = $this->storage->subscription($event->customer(), true);
 
-		if($invoice instanceof StorableInvoice)
+		if($invoice->total() > 0.00) // only when total is greater than 0
 		{
-			if($invoice->total() > 0.00) // only when total is greater than 0
+			if($invoice instanceof StorableInvoice)
 			{
 				$invoice->store($subscription['user_id']); // store invoice
 			}
+
+			if($this->cashew->user($subscription['user_id'])->hasCard() and $invoice->total() > 0.00)
+			{
+				$this->storage->updateStatus($subscription['user_id'], 'active'); // make subscription active
+			}
 		}
+
 
 		IlluminateEvent::fire('cashew.payment.success', array($subscription['user_id'], $invoice));
 	}
