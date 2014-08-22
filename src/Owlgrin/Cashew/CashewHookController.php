279293458319f21af1bcfe7e84589df51c00538b
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-23 00:44:50 +0530

    doc blocked the webhook controller

--- src/Owlgrin/Cashew/CashewHookController.php
+++ src/Owlgrin/Cashew/CashewHookController.php
@@ -8,19 +8,35 @@ use App;
 
 class CashewHookController extends Controller {
 
+	/**
+	 * The Gateway instance
+	 * @var Owlgrin\Cashew\Gateway\Gateway
+	 */
 	protected $gateway;
 
+	/**
+	 * The constructor
+	 * @param Gateway $gateway
+	 */
 	public function __construct(Gateway $gateway)
 	{
 		$this->gateway = $gateway;
 	}
 
+	/**
+	 * Hash to map type of hooks with their handlers
+	 * @var array
+	 */
 	private $hooks = array(
 		'invoice.payment_failed' => 'InvoiceFailHook',
 		'invoice.payment_succeeded' => 'InvoiceSuccessHook',
 		'customer.subscription.updated' => 'SubscriptionUpdateHook'
 	);
 
+	/**
+	 * Handles the event
+	 * @return Illuminate\Http\Response
+	 */
 	public function handle()
 	{
 		try
@@ -42,16 +58,30 @@ class CashewHookController extends Controller {
 		}
 	}
 
+	/**
+	 * Checks if the event can be handled
+	 * @param  string $type The type of event
+	 * @return boolean      Can the event be handled or not?
+	 */
 	protected function canBeHandled($type)
 	{
 		return array_key_exists($type, $this->hooks);
 	}
 
+	/**
+	 * Returns the instance of the handler for the event
+	 * @param  string $type The type of event
+	 * @return Owlgrin\Cashew\Hooks\Hook       The handler instance
+	 */
 	protected function getHook($type)
 	{
 		return App::make('Owlgrin\Cashew\Hooks\\' . $this->hooks[$type]);
 	}
 
+	/**
+	 * Returns the parsed payload from the webhook request
+	 * @return array
+	 */
 	protected function getPayload()
 	{
 		return (array) json_decode(Request::getContent(), true);
