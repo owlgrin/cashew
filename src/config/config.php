ea6d62047acc03fa8969d13f6ec2884ed7de45bf
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-24 00:20:42 +0530

    added description of config options

--- src/config/config.php
+++ src/config/config.php
@@ -1,13 +1,42 @@
 <?php
 
 return array(
+	/**
+	 * The API keys of your Stripe account
+	 */
 	'keys' => array(
+		/**
+		 * This is the secret key of your Stripe account. Please take care that
+		 * this key should never be revealed publicly.
+		 */
 		'secret' => 'your-secret-key-here',
+		/**
+		 * This is the publishable key of your Stripe account. This key can be used
+		 * on public pages, such as javascript code.
+		 */
 		'publishable' => 'your-publishable-key-here'
 	),
+	/**
+	 * The following options tell Cashew to work seamlessly with
+	 * the the storage. We use this SQL tables to record certain
+	 * information about the subscriptions.
+	 */
 	'tables' => array(
+		/**
+		 * This table is required to keep track of the
+		 * various subscriptions of our users.
+		 */
 		'subscriptions' => '_cashew_subscriptions',
+		/**
+		 * This table is required to store a copy of invoices
+		 * locally to make things faster.
+		 */
 		'invoices' => '_cashew_invoices'
 	),
+	/**
+	 * This option tells how many attempts to be made before,
+	 * expiring the user. By default, it is set to a sensible
+	 * default of 3 times.
+	 */
 	'attempts' => 3
 );
