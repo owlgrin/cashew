ef97563176b2f78c33ba67f6ceae9f486697ef95
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-27 16:20:05 +0530

    doc bloacked he card interface

--- src/Owlgrin/Cashew/Card/Card.php
+++ src/Owlgrin/Cashew/Card/Card.php
@@ -2,7 +2,19 @@
 
 use Owlgrin\Cashew\Card\Card;
 
+/**
+ * The Card contract
+ */
 interface Card {
+	/**
+	 * Returns the original object
+	 * @return mixed
+	 */
 	public function get();
+
+	/**
+	 * Returns the last four digits of the card
+	 * @return string
+	 */
 	public function lastFour();
 }
