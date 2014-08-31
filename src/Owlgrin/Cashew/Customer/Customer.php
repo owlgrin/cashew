2a1ad9653ea1d7c4896ec3b88cf302b0c2c19088
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-31 20:21:55 +0530

    doc blocked the customer contract

--- src/Owlgrin/Cashew/Customer/Customer.php
+++ src/Owlgrin/Cashew/Customer/Customer.php
@@ -1,8 +1,30 @@
 <?php namespace Owlgrin\Cashew\Customer;
 
+/**
+ * The Customer contract
+ */
 interface Customer {
+	/**
+	 * Returns the raw customer object
+	 * @return mixed
+	 */
 	public function get();
+
+	/**
+	 * Returns the unique identifier of the customer
+	 * @return string
+	 */
 	public function id();
+
+	/**
+	 * Returns the subscription object for the customer
+	 * @return Subscription
+	 */
 	public function subscription(); // subscriptions() method can be used with mutiple subscriptions in future
+
+	/**
+	 * Returns the card for the customer
+	 * @return Card
+	 */
 	public function card(); // cards() method can be used with multiple cards in future
 }
