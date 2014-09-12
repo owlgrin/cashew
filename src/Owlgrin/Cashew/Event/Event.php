92632c86c3e633ed1558ee415bb1692370cd3b5e
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-09-12 15:32:56 +0530

    docblocked the event contract

--- src/Owlgrin/Cashew/Event/Event.php
+++ src/Owlgrin/Cashew/Event/Event.php
@@ -1,11 +1,49 @@
 <?php namespace Owlgrin\Cashew\Event;
 
+/**
+ * The Event contract
+ */
 interface Event {
+	/**
+	 * Returns the raw data
+	 * @return array
+	 */
 	public function get();
+
+	/**
+	 * Returns the type of the event
+	 * @return string
+	 */
 	public function type();
+
+	/**
+	 * Returns the customer
+	 * @return string
+	 */
 	public function customer();
+
+	/**
+	 * Returns the invoice if the event is of invoice type
+	 * @return Invoice
+	 */
 	public function invoice();
+
+	/**
+	 * Returns the subscription object for the event
+	 * @return Subscription
+	 */
 	public function subscription();
+
+	/**
+	 * Returns the number of attempts the event has made
+	 * @return integer
+	 */
 	public function attempts();
+
+	/**
+	 * Decides if the event has failed more than a number of times
+	 * @param  integer $count
+	 * @return boolean
+	 */
 	public function failedMoreThan($count);
 }
