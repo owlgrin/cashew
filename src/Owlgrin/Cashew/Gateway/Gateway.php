4a673c28a5cd69e755d7019f3ec1faf903b66f77
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-09-14 01:54:13 +0530

    doc blocked the gatway contract

--- src/Owlgrin/Cashew/Gateway/Gateway.php
+++ src/Owlgrin/Cashew/Gateway/Gateway.php
@@ -1,10 +1,50 @@
 <?php namespace Owlgrin\Cashew\Gateway;
 
+/**
+ * The Gateway Contract
+ */
 interface Gateway {
+	/**
+	 * Creates a subscription
+	 * @param  array $options
+	 * @return Customer
+	 */
 	public function create($options);
+
+	/**
+	 * Updates a customer
+	 * @param  string $customer
+	 * @param  array $options
+	 * @return Customer
+	 */
 	public function update($customer, $options);
-	public function cancel($customer, $subscription);
+
+	/**
+	 * Cancels a subscription
+	 * @param  string $customer
+	 * @param  boolean $atPeriodEnd
+	 * @return Subscription
+	 */
+	public function cancel($customer, $atPeriodEnd);
+
+	/**
+	 * Returns the invoices
+	 * @param  string $customer
+	 * @return array
+	 */
 	public function invoices($customer);
+
+	/**
+	 * Returns the next invoice
+	 * @param  string $customer
+	 * @return Invoice
+	 */
 	public function nextInvoice($customer);
+
+	/**
+	 * Returns an event
+	 * @param  string $event
+	 * @return Event
+	 */
 	public function event($event);
 }
