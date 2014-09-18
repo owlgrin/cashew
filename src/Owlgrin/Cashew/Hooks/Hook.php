15c08ed0dff4153a6d747b0f77ff28ad80bceb53
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-09-18 16:30:20 +0530

    docblocked the webhook handler contract

--- src/Owlgrin/Cashew/Hooks/Hook.php
+++ src/Owlgrin/Cashew/Hooks/Hook.php
@@ -2,6 +2,14 @@
 
 use Owlgrin\Cashew\Event\Event;
 
+/**
+ * The Hook Contract
+ */
 interface Hook {
+	/**
+	 * Handles the Webhook call
+	 * @param  Event  $payload
+	 * @return mixed
+	 */
 	public function handle(Event $payload);
 }
