1fe035a50f126889e166fe7385234de370908dc3
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-09-25 18:02:54 +0530

    doc blocked local invoice implementation

--- src/Owlgrin/Cashew/Invoice/LocalInvoice.php
+++ src/Owlgrin/Cashew/Invoice/LocalInvoice.php
@@ -3,8 +3,15 @@
 use Owlgrin\Cashew\Invoice\Invoice;
 use Carbon\Carbon;
 
+/**
+ * The local implementation of invoice
+ */
 class LocalInvoice implements Invoice {
 
+	/**
+	 * The raw invoice object
+	 * @var array
+	 */
 	protected $invoice;
 
 	public function __construct($invoice)
@@ -12,26 +19,47 @@ class LocalInvoice implements Invoice {
 		$this->invoice = $invoice;
 	}
 
+	/**
+	 * Returns the identifier
+	 * @return string
+	 */
 	public function id()
 	{
 		return $this->invoice['invoice_id'];
 	}
 
+	/**
+	 * Returns the customer id
+	 * @return string
+	 */
 	public function customerId()
 	{
 		return $this->invoice['customer_id'];
 	}
 
+	/**
+	 * Returns the subscription if
+	 * @return string
+	 */
 	public function subscriptionId()
 	{
 		return $this->invoice['subscription_id'];
 	}
 
+	/**
+	 * Returns the currency
+	 * @return string
+	 */
 	public function currency()
 	{
 		return $this->invoice['currency'];
 	}
 
+	/**
+	 * Returns the date of invoice
+	 * @param  boolean $formatted
+	 * @return integer|string
+	 */
 	public function date($formatted = true)
 	{
 		return $formatted
@@ -39,6 +67,11 @@ class LocalInvoice implements Invoice {
 			: Carbon::createFromFormat('Y-m-d H:i:s', $this->invoice['date'])->getTimestamp();
 	}
 
+	/**
+	 * Returns the start of period of invoice
+	 * @param  boolean $formatted
+	 * @return integer|string
+	 */
 	public function periodStart($formatted = true)
 	{
 		return $formatted
@@ -46,6 +79,11 @@ class LocalInvoice implements Invoice {
 			: Carbon::createFromFormat('Y-m-d H:i:s', $this->invoice['period_start'])->getTimestamp();
 	}
 
+	/**
+	 * Returns the end of period of invoice
+	 * @param  boolean $formatted
+	 * @return integer|string
+	 */
 	public function periodEnd($formatted = true)
 	{
 		return $formatted
@@ -53,41 +91,74 @@ class LocalInvoice implements Invoice {
 			: Carbon::createFromFormat('Y-m-d H:i:s', $this->invoice['period_end'])->getTimestamp();
 	}
 
+	/**
+	 * Returns the total of invoice
+	 * @return number
+	 */
 	public function total()
 	{
 		return number_format($this->invoice['total'], 2);
 	}
 
+	/**
+	 * Returns the formatted total of invoice
+	 * @return string
+	 */
 	public function formattedTotal()
 	{
 		return $this->_formatted($this->total());
 	}
 
+	/**
+	 * Returns the subtotal of invoice
+	 * @return number
+	 */
 	public function subtotal()
 	{
 		return number_format($this->invoice['subtotal'], 2);
 	}
 
+	/**
+	 * Returns the formatted subtotal
+	 * @return string
+	 */
 	public function formattedSubtotal()
 	{
 		return $this->_formatted($this->subtotal());
 	}
 
+	/**
+	 * Tells if invoice has discount or not
+	 * @return boolean
+	 */
 	public function hasDiscount()
 	{
 		return $this->invoice['total'] > 0 and $this->invoice['subtotal'] != $this->invoice['total'];
 	}
 
+	/**
+	 * Returns the discount in invoice
+	 * @return number
+	 */
 	public function discount()
 	{
 		return number_format($this->subtotal() - $this->total(), 2);
 	}
 
+	/**
+	 * Returns the formatted discount in invoice
+	 * @return string
+	 */
 	public function formattedDiscount()
 	{
 		return $this->_formatted($this->discount());
 	}
 
+	/**
+	 * Formats the number
+	 * @param  number $amount
+	 * @return string
+	 */
 	private function _formatted($amount)
 	{
 		return number_format(round(money_format('%i', $amount), 2), 2);
