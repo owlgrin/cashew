73fff196e26cf6235dd2b34106a0f9bffac4f66c
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-09-27 00:25:20 +0530

    docblocked the facade

--- src/Owlgrin/Cashew/CashewFacade.php
+++ src/Owlgrin/Cashew/CashewFacade.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * The Cashew Facade
+ */
 class CashewFacade extends Facade
 {
+	/**
+	 * Returns the binding in IoC container
+	 * @return string
+	 */
 	protected static function getFacadeAccessor()
 	{
 		return 'cashew';
