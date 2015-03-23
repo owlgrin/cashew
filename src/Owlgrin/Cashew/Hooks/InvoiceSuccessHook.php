f123104756e805c34074bbaeb38a90e33cf3ddef
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-23 10:05:18 +0000

    not expiring user when invoice total is zero

--- src/Owlgrin/Cashew/Hooks/InvoiceSuccessHook.php
+++ src/Owlgrin/Cashew/Hooks/InvoiceSuccessHook.php
@@ -47,26 +47,29 @@ class InvoiceSuccessHook implements Hook {
 			$this->storage->updateStatus($subscription['user_id'], 'active'); // make subscription active
 		}
 
-		if($this->shouldBeExpired($invoice, $subscription))
-		{
-			Cashew::expireCustomer($event->customer());
+		// should NOT expire when invoice is successfully paid. even if the amount is ZERO.
+		// otherwise, how could we create 'admin' accounts
+		// 
+		// if($this->shouldBeExpired($invoice, $subscription))
+		// {
+		// 	Cashew::expireCustomer($event->customer());
 
-			IlluminateEvent::fire('cashew.user.expire', array($subscription['user_id']));
-		}
+		// 	IlluminateEvent::fire('cashew.user.expire', array($subscription['user_id']));
+		// }
 
 
 		IlluminateEvent::fire('cashew.payment.success', array($subscription['user_id'], $invoice));
 	}
 
-	private function shouldBeExpired($invoice, $subscription)
-	{
-		return (! Cashew::hasCard() and $invoice->total() == 0.00 and $this->isTrialOver($subscription));
-	}
+	// private function shouldBeExpired($invoice, $subscription)
+	// {
+	// 	return (! Cashew::hasCard() and $invoice->total() == 0.00 and $this->isTrialOver($subscription));
+	// }
 
-	private function isTrialOver($subscription)
-	{
-		if(is_null($subscription['trial_ends_at'])) return true;
+	// private function isTrialOver($subscription)
+	// {
+	// 	if(is_null($subscription['trial_ends_at'])) return true;
 
-		return Carbon::createFromFormat('Y-m-d H:i:s', $subscription['trial_ends_at'])->startOfDay()->lt(Carbon::today());
-	}
+	// 	return Carbon::createFromFormat('Y-m-d H:i:s', $subscription['trial_ends_at'])->startOfDay()->lt(Carbon::today());
+	// }
 }
